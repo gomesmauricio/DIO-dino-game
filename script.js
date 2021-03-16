18e2e9618137a6b7468fd6d29cfa2e4ab9701f95
gomesmauricio <josemauricio.gomes@gmail.com>
2021-03-16 18:04:02 -0300

    Animação do dino iniciada

--- script.js
+++ script.js
@@ -2,9 +2,31 @@ const dino = document.querySelector(".dino");
 
 function handleKeyUp(event) {
     if (event.keyCode === 32){
-        console.log('Pressionou espaço ou seta up!');
+        jump();
     }
+}
 
+function jump() {
+    let position = 0;
+    let upInterval = setInterval( () => {
+        if(position >= 150){
+            clearInterval(upInterval);
+
+            //Descendo
+            let downInterval = setInterval( () => {
+                if (position <=0){
+                    clearInterval(downInterval);
+                }else{
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                        }
+            }, 20);
+        }else {
+        //subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
